6a01cbbae41a8da46f33f11e13244972556be6b2
王浩静 <whj@wooduan.com>
2018-02-27 17:07:24 +0800

    优化路由 1.增加scrollBehavior滚动行为 2.webpack 由import方式改为独立碎片打包方式 require.ensure() 3.store增加默认代码

--- src/main.js
+++ src/main.js
@@ -1,16 +1,32 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import routes from './router'
 import store from './store'
 
+Vue.use(VueRouter);
 
-// Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+const router = new VueRouter({
+	routes,
+	scrollBehavior(to,from,savePosition) {
+		if(savePosition) {
+			return savePosition
+		} else {
+			if(from.meta.keepAlive) {
+				from.meta.savePosition = documment.body.scrollTop;
+			}
+			return {x:0,y:to.meta.savePosition || 0}
+		}
+	}
+})
+
+
 new Vue({
   router,
   store
 }).$mount('#app');
+
+
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 import App from '../App'
-import zixun from '@/views/zx'
-import zuixin from '@/views/zixun/zuixin'
-import xinwen from '@/views/zixun/xinwen'
-import zhanchang from '@/views/zc'
-import saishi from '@/views/ss'
-import zhibo from '@/views/zb'
-Vue.use(Router)
+//import zixun from '@/views/zx'
+// import zuixin from '@/views/zixun/zuixin'
+// import xinwen from '@/views/zixun/xinwen'
+// import zhanchang from '@/views/zc'
+// import saishi from '@/views/ss'
+// import zhibo from '@/views/zb'
+const helloWorld = r => require.ensure([],() => r(require('@/components/HelloWorld')),'helloWorld');
+const zixun = r => require.ensure([],() => r(require('@/views/zx')),'zixun');
+const zuixin = r => require.ensure([],() => r(require('@/views/zixun/xinwen')),'zuixin');
+const xinwen = r => require.ensure([],() => r(require('@/views/zixun/xinwen')),'xinwen');
+const zhanchang = r => require.ensure([],() => r(require('@/views/zc')),'zhanchang');
+const saishi = r => require.ensure([],() => r(require('@/views/ss')),'saishi');
+const zhibo = r => require.ensure([],() => r(require('@/views/zb')),'zhibo');
 
-export default new Router({
-  routes: [{
+export default [{
     path:'/',
     component:App,
     children:[
       {
         path: '',
         name: 'HelloWorld',
-        component: HelloWorld
+        component: helloWorld
       },{
         path: 'zixun',
         component: zixun,
@@ -47,4 +50,4 @@ export default new Router({
       }
     ]
   }]
-})
+
