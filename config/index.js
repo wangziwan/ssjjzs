2032669da068373ec560d81e5312ee2ffe30dcfd
王浩静 <whj@wooduan.com>
2018-04-09 21:02:00 +0800

    【测试】上传生成环境代码

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://wangziwan.github.io/ssjjzs/',
+    assetsPublicPath: 'https://wangziwan.github.io/ssjjzs/dist/',
 
     /**
      * Source Maps
